92915c4d5ed423d7992bb817bd1a108fa1dcc2d7
DenisChug <bestdxcom@gmail.com>
2024-10-18 15:13:07 +0300

    Update Lesson_9_7.py

--- Lesson_9_7.py
+++ Lesson_9_7.py
@@ -1,22 +1,21 @@
 def is_prime(func):
     def wrapper(*args, **kwargs):
-        if func(*args, **kwargs) > 1:
-            for i in range(2, int(func(*args, **kwargs) ** 0.5 + 1)):
-                if func(*args, **kwargs) % i == 0:
-                    print(f'Составное')
-                    break
-                else:
-                    print(f'Простое')
-                    break
-        else:
-            print(f'Составное')
+        prov = 0
+        if func(*args, **kwargs) !=0:
+            for div in range(1, func(*args, **kwargs)):
+                if func(*args, **kwargs) % div == 0 and func(*args, **kwargs) != div:
+                    prov += 1
+                    if prov > 1:
+                        print(f'Составное')
+                        break
+            if prov < 2:
+                print(f'Простое')
         return func(*args, **kwargs)
     return wrapper
 
 @is_prime
 def sum_three(*args):
     return sum(args)
-
 
 result = sum_three(2, 3, 6)
 print(result)
